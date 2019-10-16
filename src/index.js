c597419688357733fa2d1238a4d5862ffc654f6d
JanoahDev <jonah.de.smet@student.howest.be>
2019-10-16 09:57:04 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { devineLog, sum } from './js/utility/helpers.js';
+{
+    const init = () => {
+        console.log(`yeet`);
+        devineLog(`Ik gebruik de devineLog functie`);
+        sum(10, 2);
+    }
+
+    init();
+}
---
+++ src/js/utility/helpers.js
@@ -0,0 +1,5 @@
+export const devineLog = logThis => console.log(`Devine log: ${logThis}`);
+
+export const sum = (a, b) => {
+    return a + b;
+}
